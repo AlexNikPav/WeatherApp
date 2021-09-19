0382c1113407cc8dcaa16c2cb4a3a7e11bb84d5c
pavlov <pavlov@studiofact.ru>
2021-09-19 21:20:06 +0500

    Урок 2

---
+++ app/src/main/java/ru/geekbrains/weatherapp/viewmodel/MainViewModel.kt
@@ -0,0 +1,27 @@
+package ru.geekbrains.weatherapp.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.geekbrains.weatherapp.models.Repository
+import ru.geekbrains.weatherapp.models.RepositoryImpl
+
+class MainViewModel(
+    private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
+    private val repositoryImpl: Repository = RepositoryImpl()
+) :
+    ViewModel() {
+
+    fun getLiveData() = liveDataToObserve
+
+    fun getWeatherFromLocalSource() = getDataFromLocalSource()
+
+    fun getWeatherFromRemoteSource() = getDataFromLocalSource()
+
+    private fun getDataFromLocalSource() {
+        liveDataToObserve.value = AppState.Loading
+        Thread {
+            Thread.sleep(1000)
+            liveDataToObserve.postValue(AppState.Success(repositoryImpl.getWeatherFromLocalStorage()))
+        }.start()
+    }
+}
